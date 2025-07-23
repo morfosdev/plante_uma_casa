d79d668281b5d9ece5aa655e94494c9415673346
Carlos A. <carlosvsk@gmail.com>
2025-07-23 15:30:03 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -674,6 +674,34 @@ console.log({item});
         
 
         (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"ab0login",
+
+          styles:[
+        `{ width: "100%", height: "100%" }`, 
+        `{ alignItems: "center", justifyContent: "center" }`, `{ backgroundColor: "$var_all.colors.primary" }`],
+
+          screenElements:[() => {
+                  const textStyle = {
+                    fontSize: 20,
+                    color: '#fff2',
+                    textAlign:'center',
+                    maxWidth: 200,
+                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
+                  };
+
+                  return (
+                    <RN.Text style={textStyle}>
+                      {'Adicione Elementos nessa tela!'}
+                    </RN.Text>);
+                }],
+
+          functions:[()=>{}],
+
+          args,
+        }}/>, 
+        
+
+        (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"a0login",
 
           styles:[
@@ -708,40 +736,6 @@ console.log({item});
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"a1list",
-
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
-
-          screenElements:[() => {
-                  const textStyle = {
-                    fontSize: 20,
-                    color: '#fff2',
-                    textAlign:'center',
-                    maxWidth: 200,
-                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
-                  };
-
-                  return (
-                    <RN.Text style={textStyle}>
-                      {'Adicione Elementos nessa tela!'}
-                    </RN.Text>);
-                }],
-
-          functions:[()=>{}],
-
-          args,
-        }}/>, 
-        
-
-        (...args: any) => <Elements.Screen3 pass={{
-          pathScreen:"home",
 
           styles:[
               {
