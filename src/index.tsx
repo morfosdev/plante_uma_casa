86481d5623f3ddc0481cc54ff30eca8edfe17b1f
Carlos A. <carlosvsk@gmail.com>
2025-07-23 17:07:00 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -664,7 +664,8 @@ console.log({item});
         `{ flex: 1 }`, 
         `{ width: "80%", maxWidth: 280, minHeight: 150, backgroundColor: "#FFF", borderRadius: 20 }`, 
         `{ width: "fit-content", minWidth: 120, height: 30, backgroundColor: "$var_all.colors.primary", borderRadius: 20, alignItems: "center", justifyContent: "center" }`, 
-        `{ color: "#FFF" }`, `{ width: 10, height: 10 }`],
+        `{ color: "#FFF" }`, 
+        `{ width: 10, height: 10 }`, `{ backgroundColor: "$var_all.colors.primary" }`],
 
             functions:[()=>{}],            childrenItems:[() =><></>],
 
