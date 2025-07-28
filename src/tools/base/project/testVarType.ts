3bc90e6371f9c444e1329c194e8c2400d64fefff
Carlos A. <carlosvsk@gmail.com>
2025-07-28 13:59:20 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/testVarType.ts
+++ src/tools/base/project/testVarType.ts
@@ -6,17 +6,7 @@ export const testVarType = string => {
 	let newPath = '';
 	let varValue = '';
 
-	if (string.includes('$var_')){
-		varType = 'var';
-		newPath = string.replace('$var_', '');
-		varValue = getCtData(newPath);
-	}
 
-	if (string.includes('$arg_')){
-		varType = 'arg';
-		newPath = string.replace('$arg_', '');
-
-	}
 
   return varValue;
 };
