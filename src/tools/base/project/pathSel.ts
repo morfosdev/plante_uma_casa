fd79c4bf07f367288d5b744a9b40da6b26e0dc7c
Carlos A. <carlosvsk@gmail.com>
2025-07-29 18:23:35 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/pathSel.ts
+++ src/tools/base/project/pathSel.ts
@@ -1,6 +1,7 @@
 
 // ----------- set Path Selection
 export const pathSel = (obj?: any, path?: any) => {
+console.log({obj, path});
   const pathArr = path.split('.');
 
   const reduceCb = (p, c) => {
