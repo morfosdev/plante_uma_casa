53749d7f943ae744797cfb9eec7fd7631a4296c3
Carlos A. <carlosvsk@gmail.com>
2025-07-29 19:13:22 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/ImagePicker.tsx
+++ src/tools/base/Elements/ImagePicker.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { useData } from '../../../';
 import { mapElements, pathSel } from '../../base/project';
 
-type Tprops_CustomListener = {
+type Tprops = {
   pass: {
     variable: string[];
     childrenItems: any[];
@@ -17,25 +17,11 @@ type Tprops_CustomListener = {
 const css =
   'color: #54ff00; background-color: black; font-size: 11px; padding: 2px 6px; border-radius: 3px';
 
-export const Watcher = (props: Tprops_CustomListener) => {
+export const ImagePicker = (props: Tprops) => {
   // ---------- set Props
-  const { variable, childrenItems, arrFuncs, args } = props.pass;
+  const { arrFuncs, args } = props.pass;
 
-  // ---------- set Watch Data
-  const watchData = useData(ct => pathSel(ct, variable.join()));
-  args.watch = watchData; // pass values to children
 
-  React.useEffect(() => {
-    const callFn = async () => {
-      for (const currFunc of arrFuncs) await currFunc();
-    };
-    callFn().catch(initFuncsErr => console.log({ initFuncsErr }));
-
-    console.log('%c WATCHER', css, { watchData, args });
-  }, [watchData]);
-
-  // ---------- set Children Items
-  const children = mapElements(childrenItems, args);
-
-  return <>{children}</>;
+  return <></>;
 };
+
