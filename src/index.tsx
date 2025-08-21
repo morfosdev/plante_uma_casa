0c88eaa01a5d0db226cfb720ef4d6934071f8b2a
Carlos A. <carlosvsk@gmail.com>
2025-08-21 16:11:21 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1554,7 +1554,7 @@ width={14}     height={12}     fill="red"     viewBox="0 0 14 12"     {...props}
 
 console.log({mask});
 
-	tools.setData({path: "sc.A0.forms.iptsChanges.pass", value: mask });
+
 }],
 
           args,
