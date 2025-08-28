cf02f05732c852392d258641219bf71ae64a5c52
Carlos A. <carlosvsk@gmail.com>
2025-08-28 00:28:40 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2620,7 +2620,7 @@ xmlns="http://www.w3.org/2000/svg"
           ],
 
           arrStyles: [
-            `[object Object]`
+            `{color: 'yellow'}`
           ],
 
           children: [
