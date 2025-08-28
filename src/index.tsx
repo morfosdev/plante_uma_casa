35db8561fd6bf5a494a45863f65c04e471986a6b
Carlos A. <carlosvsk@gmail.com>
2025-08-28 00:19:30 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@
           import { tools } from './tools';
 
           // ---------- set Caps Inputs
-          const currRoute = 'aisc';
+          const currRoute = 'ab0login';
 
           let args: any = [];
 
@@ -2284,7 +2284,6 @@ whereConds ??`
 
           args,
         }}/>, 
-        
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"b3form",
@@ -2594,64 +2593,6 @@ xmlns="http://www.w3.org/2000/svg"
  arrFuncs: [() => console.log({args})], args,
  }}/>],
             args,
-        }}/>],
-
-          functions:[()=>{}],
-
-          args,
-        }}/>, 
-
-        (...args: any) => <Elements.Screen3 pass={{
-          pathScreen:"aisc",
-
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
-
-          screenElements:[
-        
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
-
-            functions:[async (...args) =>
- functions.funcGroup({ args, pass:{
- arrFunctions: [()=>{}]
- , trigger: 'on press'
-}})],            childrenItems:[() =><></>],
-
-            args,
-          }}/>
-        , (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
-
-          arrStyles: [
-            { color: 'black', fontSize: 12, }
-          ],
-
-          children: [
-            `Que tela Legal`
-          ],
-
-          args,
-
         }}/>],
 
           functions:[()=>{}],
