e034cb0f5649981a22f92ebb468357c694eedbce
Carlos A. <carlosvsk@gmail.com>
2025-09-25 14:18:44 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1828,7 +1828,7 @@ width={14}     height={12}     fill="red"     viewBox="0 0 14 12"     {...props}
 
           arrStyles: [
             `{
-	color: $var_all.colors.primary
+	color: "blue"
 }`
           ],
 
