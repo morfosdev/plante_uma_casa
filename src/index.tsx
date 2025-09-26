7f8d58bf6fa4f7281cf5a677529f0fbe5ee349da
Leandro <leandrowebmaster@gmail.com>
2025-09-26 15:36:44 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -4013,7 +4013,7 @@ width={14}     height={12}     fill="red"     viewBox="0 0 14 12"     {...props}
             styles:[
         `{ 
 	width: "100%",
-	maxWidth: 200,
+	maxWidth: 250,
 	minHeight: 80,
 	backgroundColor: "#FFF",
 	padding: 10,
