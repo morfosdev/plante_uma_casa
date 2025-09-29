3784f8459c0f5d8d4904f446e44fff9d50190fa7
Carlos A. <carlosvsk@gmail.com>
2025-09-29 15:04:44 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/Accordion.tsx
+++ src/tools/base/Elements/Accordion.tsx
@@ -2,8 +2,15 @@
 import React from 'react';
 import * as RN from 'react-native';
 
-export const Accordion = (props) => {
+export const Accordion = props => {
+  const { arrElements } = props.pass;
 
-console.log("Accordion", {props});
-  return <>Oi Accordion!!</>
-}
+  console.log('Accordion', { props });
+  return (
+    <>
+      {arrElements.map((Item, idx) => {
+        return Item();
+      })}
+    </>
+  );
+};
