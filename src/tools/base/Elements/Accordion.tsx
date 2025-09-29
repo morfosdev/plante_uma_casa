eab84afcdcd76cbb745b4c89c19f0e2488c568a7
Carlos A. <carlosvsk@gmail.com>
2025-09-29 15:27:54 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/Accordion.tsx
+++ src/tools/base/Elements/Accordion.tsx
@@ -3,40 +3,34 @@ import React from 'react';
 import { Pressable, View } from 'react-native';
 
 export const Accordion = props => {
-  const [open, setOpen] = React.useState(false);
   const { configs, arrElements } = props.pass;
 
   console.log('Accordion', { props });
   return (
     <>
       {arrElements.map((Item, idx) => {
-        return (
-          <Pressable key={idx} onPress={() => setOpen(e => !e)}>
-            <ItemAcc open={open}>{Item()}</ItemAcc>
-          </Pressable>
-        );
+        return <ItemAcc key={idx}>{Item()}</ItemAcc>;
       })}
     </>
   );
 };
 
 const ItemAcc = props => {
-  const { open } = props;
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <View
-      style={{
-        backgroundColor: 'red',
-        width: 260,
-				minHeight: 30,
-				alignItems: 'center',
-        height: open ? 'auto' : 30,
-        borderRadius: 10,
-				marginBottom: 14,
-        overflow: 'hidden',
-      }}
-    >
-      {props.children}
-    </View>
+    <Pressable onPress={() => setOpen(e => !e)}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 260,
+          height: open ? 'auto' : 30,
+          borderRadius: 10,
+          overflow: 'hidden',
+        }}
+      >
+        {props.children}
+      </View>
+    </Pressable>
   );
 };
-
