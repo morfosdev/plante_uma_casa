6d5ba470db9109370a229bc86f45d9e497099499
Leandro <leandrowebmaster@gmail.com>
2025-10-03 13:20:30 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -3961,7 +3961,7 @@ backgroundColor: 'transparent',
 	borderColor: '#e6e7e8',
 }],
 
-      URIvariablePath:["https://images.unsplash.com/photo-1488831861984-179da3647265?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+      URIvariablePath:[`https://www.condominioterranovabauru.com.br/cdn/fotos/portaria_terra_nova_bauru.jpg`],
 
       args,
     }}/>],
