1ff8b5b25ea7c25886f20d1857650d62ddb4005c
Leandro <leandrowebmaster@gmail.com>
2025-10-10 10:22:48 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -4671,9 +4671,14 @@ flexDirection: 'row',
         functions.setVar({ args, pass:{
           keyPath: [`sc.a1.iptChanges`],
           value: [undefined]
-        }}), async (...args) =>
+        }}), 
+        async (...args) =>
         functions.setVar({ args, pass:{
           keyPath: [`all.toggles.sideRight`],
+          value: [false]
+        }}), async (...args) =>
+        functions.setVar({ args, pass:{
+          keyPath: [`all.toggles.a1.add`],
           value: [false]
         }})],
         }})]
