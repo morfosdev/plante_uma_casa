668b085cd60cfa12d69e4bb4796e3ddd5a92dd0e
Leandro <leandrowebmaster@gmail.com>
2025-10-13 17:15:02 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@
           import { tools } from './tools';
 
           // ---------- set Caps Inputs
-          const currRoute = 'ab0login';
+          const currRoute = 'a1list';
 
           let args: any = [];
 
