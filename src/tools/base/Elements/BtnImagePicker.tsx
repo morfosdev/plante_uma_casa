207dbfef686fe1a59da5f9e994f65411996e73d3
Carlos A. <carlosvsk@gmail.com>
2025-10-15 11:10:57 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/BtnImagePicker.tsx
+++ src/tools/base/Elements/BtnImagePicker.tsx
@@ -54,7 +54,9 @@ const BtnImgPicWeb = (props: any) => {
   return (
     <>
       <RN.View style={styles.container}>
-        <RN.Button title="Escolher imagem" onPress={() => pickWeb()} />
+        <RN.Pressable style={styles.btn1} onPress={() => pickWeb()}>
+          <RN.Text style={styles.txt1}>Adicionar</RN.Text>
+        </RN.Pressable>
         {image && <RN.Image source={{ uri: image }} style={styles.image} />}
         <RN.Text style={styles.txt1}>Adicionar Imagens</RN.Text>
         <RN.Text style={styles.txt2}>
