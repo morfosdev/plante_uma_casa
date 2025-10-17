86332967ded44bd4c8946e8d3a5ac1e0b083428a
Renan <72417996+Rehito0@users.noreply.github.com>
2025-10-17 08:56:40 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -15924,7 +15924,10 @@ async (...args) =>
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[`{ width: "100%", height: "100%" }`],
+            styles:[`{ 
+flex: 1, 
+height: "100%",
+}`],
 
             functions:[()=>{}],            childrenItems:[
         
