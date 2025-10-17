d3766ca0a72be704430ade6d8bffb524e1c329de
Renan <72417996+Rehito0@users.noreply.github.com>
2025-10-17 08:51:24 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -16072,9 +16072,15 @@ async (...args) =>
             elementsProperties:['{}'],
 
             styles:[`{
-	width: 200,
 	height: 30,
-	backgroundColor: 'blue',
+	backgroundColor: 'transparent',
+	paddingHorizontal: 30,
+	paddingVertical: 8,
+	borderWidth: 2,
+	borderRadius: 20,
+	borderColor: 'white',
+	justifyContent: 'center',
+	alignItems: 'center',
 }`],
 
             functions:[async (...args) =>
@@ -16096,11 +16102,15 @@ async (...args) =>
           ],
 
           arrStyles: [
-            { color: 'black', fontSize: 12, }
+            `{
+	fontSize: 15,
+	fontWeight: '700',
+	color: 'white',
+}`
           ],
 
           children: [
-            `Adicionar`
+            `Label`
           ],
 
           args,
