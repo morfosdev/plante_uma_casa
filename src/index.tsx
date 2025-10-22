9ca63a787875676d13021b113ebb7358bff5354c
Leandro <leandrowebmaster@gmail.com>
2025-10-22 10:56:35 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -41032,7 +41032,7 @@ width={14}     height={12}     fill="red"     viewBox="0 0 14 12"     {...props}
           ],
 
           children: [
-            `Alphaville PE1`
+            `R$ 2.200,00`
           ],
 
           args,
