313c3c32e10dc30a913e67028b39bae0dac4ed29
Leandro <leandrowebmaster@gmail.com>
2025-10-31 09:04:11 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -5373,8 +5373,8 @@ width: 15,
  arrFunctions: [
 async (...args) =>
         functions.setVar({ args, pass:{
-          keyPath: [`sc.A7.forms.iptsChanges.condId`],
-          value: [`$arg_item.docId`]
+          keyPath: [`sc.A7.forms.iptsChanges.condoData`],
+          value: [`$arg_item`]
         }}), 
         (...args) => {
           // ---------- get Function from A_Project Scope
