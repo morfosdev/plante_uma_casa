3dba0fe6bd7a701bf74bd867f003f2edad464e5b
Renan <72417996+Rehito0@users.noreply.github.com>
2025-11-17 12:27:24 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -30635,7 +30635,7 @@ async (...args) =>
           value: [false]
         }}), async (...args) =>
         functions.setVar({ args, pass:{
-          keyPath: [`all.toggles.a1.add`],
+          keyPath: [`all.toggles.a1.editCondo`],
           value: [false]
         }})]
  , trigger: 'on press'
