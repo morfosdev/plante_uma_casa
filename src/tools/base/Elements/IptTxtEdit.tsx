517b55fadbf831594b9eea5f31a1eab270a9d108
Carlos A. <carlosvsk@gmail.com>
2025-11-18 13:40:13 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/IptTxtEdit.tsx
+++ src/tools/base/Elements/IptTxtEdit.tsx
@@ -59,6 +59,7 @@ export const IptTxtEdit = (props: Tprops) => {
 
     // modo externo: mantém input responsivo e sincroniza store
     setText(val); // feedback imediato
+setData({ path: joinedPath, value: val });
     for (const fn of funcsArray) {
       await fn(val, args);
     }
