203990159d27fe0dac8c8d4c4756caaab216ff89
Renan <72417996+Rehito0@users.noreply.github.com>
2025-11-21 08:44:01 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -31011,25 +31011,6 @@ async (...args) =>
             functions:[()=>{}],            childrenItems:[
         
 
-    (...args:any) => <Elements.ImageBox pass={{
-      elementsProperties:[{}],
-
-      styles:[
-              {
-                height: "50px",
-                width: "80px",
-                backgroundColor: "#FDFDFD",
-                borderWidth: 2,
-                borderColor: "#EEE"
-              }
-              ],
-
-      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/projeto-plante-uma-casa.firebasestorage.app/o/images%2Fimage_44.png?alt=media&token=e7415ce8-af35-476d-97bc-293919f99949`],
-
-      args,
-    }}/>, 
-        
-
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
@@ -31157,6 +31138,7 @@ async (...args) =>
             args,
           }}/>
         , 
+        
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
@@ -31209,7 +31191,23 @@ async (...args) =>
 
             args,
           }}/>
-        ],
+        , 
+
+    (...args:any) => <Elements.ImageBox pass={{
+      elementsProperties:[{}],
+
+      styles:[{
+height: 100,
+width: '100%',
+position: 'absolute',
+zIndex: -1,
+right: 2,
+}],
+
+      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/projeto-plante-uma-casa.firebasestorage.app/o/images%2Fimage_44.png?alt=media&token=e7415ce8-af35-476d-97bc-293919f99949`],
+
+      args,
+    }}/>],
 
             args,
           }}/>
