5c2095910417ebeec6e308a79f5010e9eb1668ff
Carlos A. <carlosvsk@gmail.com>
2025-12-03 15:00:57 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -66419,22 +66419,6 @@ fontWeight: '700',
 
           itemElements: [
             
-        (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
-
-          arrStyles: [
-            { color: 'black', fontSize: 12, }
-          ],
-
-          children: [
-            "Escreva..."
-          ],
-
-          args,
-
-        }}/>, 
 
     (...args:any) => <Elements.ImageBox pass={{
       elementsProperties:[{}],
