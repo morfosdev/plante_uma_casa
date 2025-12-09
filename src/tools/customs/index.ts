cf8529950b5949ae53bdff41d7a5de335d15b00b
Carlos A. <carlosvsk@gmail.com>
2025-12-09 11:21:45 -0300

    Commit generated by Flaxboll.

--- src/tools/customs/index.ts
+++ src/tools/customs/index.ts
@@ -2,25 +2,22 @@
 // ---------- import Local Tools
 
 // ----------- JS Functions
-
-export { checkCurrSecondsValue } from './checkCurrSecondsValue';
-export { processCheckout } from './processCheckout';
-export { currencyString } from './currencyString';
-export { checkInitData } from './checkInitData';
+export { usePushNotifications } from './usePushNotifications';
+// export { checkCurrSecondsValue } from './checkCurrSecondsValue';
+// export { checkInitData } from './checkInitData';
+// export { currencyString } from './currencyString';
+// export { processCheckout } from './processCheckout';
 
 // ----- generic names
 
 // ----------- JSX
+// export { BtnUpdateAmount } from './BtnUpdateAmount';
 
-export { BtnUpdateAmount } from './BtnUpdateAmount';
-
-export { CountTimer } from './CountTimer';
-export { MapPlaces } from './MapPlaces';
-export { IptPicker } from './IptPicker';
-export { Shadows } from './Shadows';
-export { Watcher } from './Watcher';
+// export { CountTimer } from './CountTimer';
+// export { IptPicker } from './IptPicker';
+// export { MapPlaces } from './MapPlaces';
+// export { Shadows } from './Shadows';
+// export { Watcher } from './Watcher';
 
 // ----- generic names
-
-export { FilePicker } from './FilePicker';
-
+// export { FilePicker } from './FilePicker';
