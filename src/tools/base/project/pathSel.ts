7c51f2d6779c75f3c493140850fe76f22d784bc5
Carlos A. <carlosvsk@gmail.com>
2026-01-07 16:33:08 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/pathSel.ts
+++ src/tools/base/project/pathSel.ts
@@ -1,7 +1,6 @@
 
 // ----------- set Path Selection (defensivo)
 export const pathSel = (obj?: any, path?: any) => {
-console.log({obj, path});
   if (typeof path !== 'string' || !path) return undefined;
 
   const parts = path.split('.').filter(Boolean);
