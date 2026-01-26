e30c54ddafdd9859a8e4d77e079feee40e276c90
Carlos A. <carlosvsk@gmail.com>
2026-01-26 16:49:23 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -83595,7 +83595,7 @@ tools.setData({
 }, 
 'colors': { 
 'primary': '#315E2D', 
-'secondary': "#0064fe", 'smoke': "#ddd" } 
+'secondary': "#0064fe", 'smoke': "#b3b5a8" } 
 , 
 'lists': { 
 'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}], 
