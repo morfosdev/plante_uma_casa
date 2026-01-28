a4dbf120118e1c97c441e9baa53394d79290169a
Carlos <carlosvsk@gmail.com>
2026-01-28 12:35:56 -0300

    add: app redirects when login user

--- App.tsx
+++ App.tsx
@@ -1,16 +1,47 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Router } from "./src";
+import { Platform, StyleSheet, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Router, useData } from "./src";
+import { tools } from "./src/tools";
 
 export default function App() {
+  const fullRegister = useData((ct) => ct.all.authUser?.fullRegister);
+
   React.useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.addEventListener("controllerchange", () => {
-        window.location.reload();
-      });
+    const auth = getAuth();
+
+    const unsubAuth = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log({ fullRegister });
+        tools.goTo(fullRegister ? "c5steps" : "c2register");
+      } else {
+        tools.goTo("c1login");
+      }
+    });
+
+    let onControllerChange: (() => void) | null = null;
+
+    if (Platform.OS === "web" && "serviceWorker" in navigator) {
+      onControllerChange = () => window.location.reload();
+      navigator.serviceWorker.addEventListener(
+        "controllerchange",
+        onControllerChange,
+      );
     }
-  }, []);
+
+    return () => {
+      unsubAuth();
+      if (onControllerChange) {
+        navigator.serviceWorker.removeEventListener(
+          "controllerchange",
+          onControllerChange,
+        );
+      }
+    };
+  }, [fullRegister]);
+
   return (
     <View style={styles.container}>
       <Router />
